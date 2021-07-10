a20c4cd8f0ea4d25021aeb57ef54955ea8e8c100
Subray <edmmyrage@gmail.com>
2021-07-11 02:43:43 +0300

    Upd

--- termux/compilekernel.sh
+++ termux/compilekernel.sh
@@ -3,7 +3,7 @@ export KBUILD_BUILD_USER=xyzuan
 export KBUILD_BUILD_HOST=xyzscape
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc) O=out ARCH=arm64 perf_defconfig
     make -j$(nproc) ARCH=arm64 O=out \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
